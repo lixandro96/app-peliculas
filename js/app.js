ecf8c591dd02e3f7eb59526df17a23afc60ffbd2
lixandro96 <80864384+lixandro96@users.noreply.github.com>
2023-02-09 21:51:42 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,50 +1,98 @@
-const contenedorPeliculas = document.querySelector('.movies');
-const imagen = 'https://image.tmdb.org/t/p/original'
+const movies = document.querySelector('.movies');
+const imagen = 'https://image.tmdb.org/t/p/w500';
+let Page = 1
+const baseUrl = 'https://api.themoviedb.org/3'
+const apiKey = '&api_key=92cd1c9da3c801f9d8bc1853983f91b1'
+const popularMovies = '/discover/movie?sort_by=popularity.desc&'
+const apiUrl = baseUrl+popularMovies+'page='+Page+apiKey;
+const populars = document.querySelector('#populars');
+
 // cargar peliculas
-document.addEventListener('DOMContentLoaded',cargarInfo)
+document.addEventListener('DOMContentLoaded',loadInfo(apiUrl));
 
-function cargarInfo(){
 
-    const url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=92cd1c9da3c801f9d8bc1853983f91b1';
-    fetch(url)
+const currentpage =document.querySelector('.currentpage');
+const lastpage = document.querySelector('.lastpage');
+const queryGenre = `/genre/movie/list?`
+const inputSearch = document.querySelector('#search');
+const form = document.querySelector('.form');
+
+currentpage.textContent= Page;
+
+
+
+function loadInfo(url){
+    try {
+        fetch(url)
         .then(response => response.json())
-        .then(data => mostrarDatos(data.results))
+        .then(data => showMovies(data))
+    } catch (error) {
+        console.log(error)
+    }
 
 }
-let pagination = 1
+
+function loadCategorie(category){
+
+    try {
+        fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=92cd1c9da3c801f9d8bc1853983f91b1&language=en-US&page=${Page}`)
+        .then(response => response.json())
+        .then(data => showMovies(data))
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+const categories = document.querySelector('.menu');
+
+categories.addEventListener("click", e =>{
+    e.preventDefault()
+    if (e.target.classList.contains('genre')){
+        console.log(e.target.id)
+        loadCategorie(e.target.id)
+    }
+})
 const next = document.querySelector('.next');
-next.addEventListener("click", ()=>{
-    next.preventDefault()
-    pagination++
-    console.log(pagination)
-    })
-prueba()
-function prueba(){
 
-    const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=${pagination}&api_key=92cd1c9da3c801f9d8bc1853983f91b1`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data =>console.log(data))
-}
+next.addEventListener("click", (e)=>{
+    Page++
+    loadInfo(baseUrl+popularMovies+'page='+Page+apiKey)
+})
 
-function mostrarDatos(informacion){
+form.addEventListener('submit',(e)=> {
+    e.preventDefault()
+    if(inputSearch.value === ''){
+        return loadInfo(baseUrl+popularMovies+'page='+Page+apiKey)
+    }
+    const urlSearch = `https://api.themoviedb.org/3/search/movie?api_key=92cd1c9da3c801f9d8bc1853983f91b1&language=en-US&page=${Page}&include_adult=true&query=${inputSearch.value}`;
 
-    informacion.forEach(element => {
-        const{title,backdrop_path,poster_path, overview,release_date,vote_average,id}= element
+    loadInfo(urlSearch)
+
+})
+
+function showMovies(informacion){
+
+    const moviesContainer = document.querySelector('.movies-container');
+    moviesContainer.innerHTML = `<section class="movies"></section>`;
+    const movies = document.querySelector('.movies');
+    informacion.results.forEach(element => {
+        const{title,backdrop_path,poster_path, overview,release_date,vote_average,id}= element;
         const card = document.createElement("div");
         card.classList.add("card")
-        const div = document.createElement('div');
-        div.classList.add('movie');
-        div.innerHTML = `<img src=${imagen+poster_path} class='poster'>`;
+        card.innerHTML = `
 
-        const info = document.createElement("div");
-        info.classList.add("info")
-        info.innerHTML = `<h3 class="title">${title}
-            <span>(${release_date})</span>
-          </h3>`
-        card.appendChild(div)
-        card.appendChild(info)
-        contenedorPeliculas.appendChild(card)
+            <div class="movie">
+                <span class="vote_average">${vote_average}</span>
+                <img src=${imagen+poster_path} alt='${title}' class='poster'>
+                <div class="info">
+                    <h3 class="title">${title}<span>(${release_date})</span></h3>
+                </div>
+            </div>
+        `;
+
+        movies.appendChild(card)
+        currentpage.textContent= Page
+        lastpage.textContent = informacion.total_pages
     });
 }
 
@@ -57,6 +105,4 @@ menuBtn.addEventListener('click', () =>{
     bars[0].classList.toggle('bar_transform1')
     bars[1].classList.toggle('bar_hidden')
     bars[2].classList.toggle('bar_transform2')
-
-
 })
